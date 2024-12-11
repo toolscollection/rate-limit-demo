14d62f1b91fffcf4c4618a7485d89b680a761477
lect <lec@test.com>
2024-12-11 11:23:22 +0800

    首次提交

---
+++ src/main/java/com/futuretech/RateLimitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.futuretech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/futuretech/ratelimitdemo/RateLimitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuretech.ratelimitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
